f642b7abb4e771c43694f76a8bea30d525ee8a93
Trixen99 <NathanWhitehead99@hotmail.com>
2024-11-22 23:28:13 +0000

    added split_nodes_delimiter, added the spliiter function

--- src/my_test.py
+++ src/my_test.py
@@ -1,10 +1,16 @@
 from htmlnode import *
 from textnode import *
+from delimiter import *
 
-#node = TextNode("hello",TextType.IMAGE, "www.themain.com")
+node = TextNode("This is text with a `code block` word", TextType.TEXT)
+node2 = TextNode("This is text with a `code block` word number 2", TextType.BOLD)
+node3 = TextNode("This is text with a code block word number `2`", TextType.BOLD)
+#new_nodes = split_nodes_delimiter([node3], "`", TextType.CODE)
+new_nodes = split_nodes_delimiter([node, node2], "`", TextType.CODE)
+
+print(f"node\n{node}")
+print(f"new nodes\n{new_nodes}")
 
 
-#print(node)
 
-#print(text_node_to_html_node(node))
 
