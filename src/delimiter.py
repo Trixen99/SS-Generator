3db2d8fe8907a5aac1fac7291cad16d71d5947de
Trixen99 <NathanWhitehead99@hotmail.com>
2024-11-23 21:30:06 +0000

    removed splitter function going to rewrite it later

--- src/delimiter.py
+++ src/delimiter.py
@@ -1,20 +1,10 @@
+from textnode import *
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    def splitter(old_nodes, delimiter):
-        text_from_node_list = old_nodes[0].text.split(delimiter)
-        if len(old_nodes) <= 1:
-            return text_from_node_list
-        other_nodes = splitter(old_nodes[1:], delimiter)
-        print(f"other_nodes\n{other_nodes}")
-        text_from_node_list.extend(other_nodes)
-        return text_from_node_list
 
-    def formatter(split_text, text_type):
-        
-        return split_text
+                  
 
 
-    split_text = splitter(old_nodes, delimiter)
-    formatted_text = formatter(split_text, text_type)
-    return formatted_text
+    
+
--- src/my_test.py
+++ src/my_test.py
@@ -5,10 +5,8 @@ from delimiter import *
 node = TextNode("This is text with a `code block` word", TextType.TEXT)
 node2 = TextNode("This is text with a `code block` word number 2", TextType.BOLD)
 node3 = TextNode("This is text with a code block word number `2`", TextType.BOLD)
-#new_nodes = split_nodes_delimiter([node3], "`", TextType.CODE)
 new_nodes = split_nodes_delimiter([node, node2], "`", TextType.CODE)
 
-print(f"node\n{node}")
 print(f"new nodes\n{new_nodes}")
 
 
