611a27455487ff286a57bfb08c3b993793c5fecf
Trixen99 <NathanWhitehead99@hotmail.com>
2024-11-25 23:16:09 +0000

    added split_node_delimitter function back in

--- src/delimiter.py
+++ src/delimiter.py
@@ -2,6 +2,48 @@ from textnode import *
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    final_list = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            final_list.append(node)
+            continue
+        split_node = node.text.split(delimiter)
+        if len(split_node) % 2 == 0:
+            raise ValueError("original text is not formatted correctly")
+        count = 1
+        split_nodes_list = []
+        for string in split_node:
+            if string == "":
+              count += 1
+              continue
+            elif count % 2 == 0:
+                split_nodes_list.append(TextNode(string,text_type))
+                count += 1
+                continue
+            split_nodes_list.append(TextNode(string, node.text_type))
+            count += 1
+            continue
+
+    final_list.extend(split_nodes_list)
+    return final_list
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
                   
 
--- src/my_test.py
+++ src/my_test.py
@@ -2,13 +2,18 @@ from htmlnode import *
 from textnode import *
 from delimiter import *
 
-node = TextNode("This is text with a `code block` word", TextType.TEXT)
-node2 = TextNode("This is text with a `code block` word number 2", TextType.BOLD)
-node3 = TextNode("This is text with a code block word number `2`", TextType.BOLD)
-new_nodes = split_nodes_delimiter([node, node2], "`", TextType.CODE)
+#node = TextNode("This is text with a `code block` word", TextType.TEXT)
+#node2 = TextNode("This is text with a `code block` word number 2", TextType.BOLD)
+#node3 = TextNode("This is text with a code block word number `2`", TextType.BOLD)
+#new_nodes = split_nodes_delimiter([node, node2], "`", TextType.CODE)
+
+#print(f"new nodes\n{new_nodes}")
+
+
+node = TextNode("This `is text` with a `code block` word", TextType.TEXT)
+new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
 
 print(f"new nodes\n{new_nodes}")
-
 
 
 
