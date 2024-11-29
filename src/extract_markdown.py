15b493a524e043b7304ff8ae4ab9ce4749e751c5
Trixen99 <NathanWhitehead99@hotmail.com>
2024-11-29 18:10:04 +0000

    finished the extract_markdowns - second try

--- src/extract_markdown.py
+++ src/extract_markdown.py
@@ -5,7 +5,7 @@ def extract_markdown_images(text):
 
 
 def extract_markdown_links(text):
-    return re.findall(r"(?<!!)\[([^[\]]+|[[\]])\]\(([^\(\)]*)\)", text)
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     
 
 
