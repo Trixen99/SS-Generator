568faf4dd30fd46d2fb3168a657bd00a98a76db1
Trixen99 <NathanWhitehead99@hotmail.com>
2024-12-12 21:23:24 +0000

    finished the two split_node functions, still need to write some tests

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -4,34 +4,21 @@ from extract_markdown import *
 
 def split_nodes_image(old_nodes):
     text_nodes = []
-    extracted_images = None
+    images = None
+    split = "IMAGE"
     for node in old_nodes:
-        extracted_images = extract_markdown_images(node.text)
-        if len(extracted_images) == 0:
-            return node
+        images = extract_markdown_images(node.text)
+        if len(images) == 0:
+            text_nodes.append(node)
+            continue
         extracted_text = node.text
-        
-        for image in extracted_images:
-            if isinstance(extracted_text,list):
-                e_text = extracted_text[-1].split(f"![{image[0]}]({image[1]})")
-                extracted_text.remove(extracted_text[-1])
-                extracted_text.extend(e_text)
-            else:
-                extracted_text = extracted_text.split(f"![{image[0]}]({image[1]})")
-        for number in range(len(extracted_text)):
-            if (extracted_text[number]) == "":
-                extracted_text.pop(number)
-        image_count = 0
-        for text_number in range(len(extracted_text)):
-            new_nodes = TextNode(extracted_text[text_number],TextType.TEXT)
-            image_node = TextNode(extracted_images[image_count][0],TextType.IMAGE,str(extracted_images[image_count][1]))
-            text_nodes.append(new_nodes)
-            text_nodes.append(image_node)
-            image_count += 1
+        text_nodes.extend(split_part_two(extracted_text, images, split))
     return text_nodes
+
 
 def split_nodes_link(old_nodes):
     text_nodes = []
+    split = "LINK"
     links = None
     for node in old_nodes:
         links = extract_markdown_links(node.text)
@@ -39,65 +26,27 @@ def split_nodes_link(old_nodes):
             text_nodes.append(node)
             continue
         extracted_text = node.text
+        text_nodes.extend(split_part_two(extracted_text, links, split))
+    return text_nodes
         
-        for link in links:
-            split_text = extracted_text.split(f"[{link[0]}]({link[1]})")
+
+
+def split_part_two(extracted_text, iterables, split):
+        node_list = []
+        for iterable in iterables:
+            split_text = None
+            new_text_type = None
+            if split == "IMAGE":
+                    split_text = extracted_text.split(f"![{iterable[0]}]({iterable[1]})")
+                    new_text_type = TextType.IMAGE
+            else:
+                    split_text = extracted_text.split(f"[{iterable[0]}]({iterable[1]})")
+                    new_text_type = TextType.LINK
+
             if split_text[0] != "":
-                text_nodes.append(TextNode(split_text[0],TextType.TEXT))
-            text_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+                node_list.append(TextNode(split_text[0],TextType.TEXT))
+            node_list.append(TextNode(iterable[0], new_text_type, iterable[1]))
             extracted_text = split_text[1]
         if extracted_text != "":
-            text_nodes.append(TextNode(extracted_text, TextType.TEXT))
-
-
-
-
-
-  
-        #if len(extracted_text) == len(links) + 1:
-            #for number in range(len(links)):
-             #   new_TEXTNode_1 = TextNode(extracted_text[number],TextType.TEXT)
-             #   new_LINKNode = TextNode(links[number][0],TextType.LINK,str(links[number][1]))
-             #   if number + 1 == len(links):
-            #        new_TEXTNode_2 = TextNode(extracted_text[number + 1],TextType.TEXT)
-           #         text_nodes.extend([new_TEXTNode_1, new_LINKNode, new_TEXTNode_2])
-          #      else:
-         #           text_nodes.extend([new_TEXTNode_1, new_LINKNode])
-
-        #e#lif len(extracted_text) == len(links):
-        #    link_count = 0
-         #   for text_number in range(len(extracted_text)):
-          #      new_nodes = TextNode(extracted_text[text_number],TextType.TEXT)
-         #       link_node = TextNode(links[link_count][0],TextType.LINK,str(links[link_count][1]))
-         #       text_nodes.append(new_nodes)
-         #       text_nodes.append(link_node)
-         #       link_count += 1
-    return text_nodes
-            
-
-
-
-        #link_count = 0
-        #for text_number in range(len(extracted_text)):
-        #    try:
-        #        new_nodes = TextNode(extracted_text[text_number],TextType.TEXT)
-        #        link_node = TextNode(extracted_links[link_count][0],TextType.LINK,str(extracted_links[link_count][1]))
-        #        text_nodes.append(new_nodes)
-        #        text_nodes.append(link_node)
-        #        link_count += 1
-        #    except:
-         #       new_nodes = TextNode(extracted_text[text_number],TextType.TEXT)
-         #       text_nodes.append(new_nodes)
-         #       link_count += 1
-
-
- 
-
-
-        
-
-
-
-    #for node in old_nodes:
-    #    link_url = extract_markdown_links(node.text)
-    #    print(link_url)
+            node_list.append(TextNode(extracted_text, TextType.TEXT))
+        return(node_list)
